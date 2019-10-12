b858e9213e236c1f4d40a126a43357a8a5637a59
League Student <league@iMac-18.attlocal.net>
2019-10-12 12:58:36 -0700

    amazing oof
    :eggplant:

--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -12,54 +12,74 @@ public class DragonFight {
 		
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
-	
+		int playerHealth = 100;
 		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
-		
+		int dragonHealth = 100;
 		// 4. Create a variable to hold the damage the player's attack does each round
-		
+		int damage = 0;
 		// 5. Create a variable to hold the damage the dragon's attack does each round
-		
+		int dragonDamage = 0;
 		
 		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
+		while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
 		
 		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
 		
 		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell or a kick
+		String attac = JOptionPane.showInputDialog("u moost attac! u want to attac with pressure or abuse?");
 		
 		// 9. If they typed in "yell":
-		
-			//-- Find a random number between 0 and 10 and store it in dragonDamage
+		String pressure = "pressure";
+		String abuse = "abuse";
+		//-- Find a random number between 0 and 10 and store it in dragonDamage
+			if (attac.equals (pressure)) {
+			Random ran = new Random();
+			int number = ran.nextInt(10);
+			dragonDamage = number;
 			
 			//-- Subtract that number from the dragon's health variable 
-			
+			dragonHealth = dragonHealth - dragonDamage;
+			}
 		// 10. If they typed in "kick":
 		
 			//-- Find a random number between 0 and 25 and store it in dragonDamage
+			if (attac.equals (abuse)) {
+			Random o = new Random();
+			int oo = o.nextInt(25);
+			dragonDamage = oo;
 			
 			//-- Subtract that number from the dragon's health variable
+			dragonHealth = dragonHealth - dragonDamage;
 			
-			
-		
+			}
 		// 11.  Find a random number between 0 and 35 and store it in playerDamage
-		
+		Random boop = new Random();
+		int boo = boop.nextInt(35);
+			damage = boo;
 		// 12. Subtract this number from the player's health
+		playerHealth = playerHealth - boo;
 		
 		
 		
 		// 13. If the user's health is less than or equal to 0
-		
+		if (playerHealth < 1) {
+			JOptionPane.showMessageDialog(null, "you are ultra fat");
+		}
 			//-- Tell the user that they lost
 			
 		
 		// 14. Else if the dragon's health is less than or equal to 0
-		
+		if (dragonHealth < 1) {
+			JOptionPane.showMessageDialog(null, "you are ultra good at bullying dragons!");
+		}
 			//-- Tell the user that the dragon is dead and they took a ton of gold!
 			
 	   //  15.  Else
 			
 			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
-			
+			 if (dragonHealth > 0) {
+			JOptionPane.showMessageDialog(null, "Your health is " + playerHealth + ". The dragon's health is " + dragonHealth + ".");
+			}
 		}
 	}
-
+}
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,44 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int boi = 0;
 		// 2.  Ask the user a question 
+		String bruh = JOptionPane.showInputDialog("what is 2 + 2");
+		// 3.  Use an if statement to check if their answer is 
+		//correct
+		if (bruh.equals("4")) {
+			
 		
-		// 3.  Use an if statement to check if their answer is correct
+		// 4.  if the user's answer was correct, add one to their 
+		//score 
+		boi = boi + 1;
+		}
+		// 5.  Create more questions by repeating steps 1, 2, and 
+		//3 below. 
+		String bruhh = JOptionPane.showInputDialog("what is 5 + 5?");
+		if (bruhh.equals("10")) {
+			boi = boi + 1;
+		}
+		String bruhhh = JOptionPane.showInputDialog("what is 5 multiplied by pi?");
+		if (bruhhh.equals("1")) {
+			boi = boi + 1;
+		}
+		// 6.  After all the questions have been asked, print the 
+		//user's score 
+		JOptionPane.showMessageDialog(null, "your score is " + boi + " out of 3!");
 		
-		// 4.  if the user's answer was correct, add one to their score 
 		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
 		
-		// 6.  After all the questions have been asked, print the user's score 
 		
+		
+		
+		
+		//end
 	}
 }
